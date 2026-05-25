b349bfc0ca933e008a77f9805eddc8c0afca5875
Adriano Dias de Sousa <adrianodias_sma@hotmail.com>
2026-05-25 05:27:33 -0300

    Aponta frontend para API online

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://clickbus-burguer-api.onrender.com/api";
 
 async function fazerRequisicaoApi(caminho, opcoes = {}) {
   const resposta = await fetch(`${API_URL}${caminho}`, {
