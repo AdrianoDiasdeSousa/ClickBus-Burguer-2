e73e0868bc5fd791d2d4eae820cb6612b2b71664
AdrianoDiasdeSousa <adrianodias_sma@hotmail.com>
2026-05-27 15:44:42 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,8 @@ const pool = require("./config/db");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "8mb" }));
+app.use(express.urlencoded({ extended: true, limit: "8mb" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
