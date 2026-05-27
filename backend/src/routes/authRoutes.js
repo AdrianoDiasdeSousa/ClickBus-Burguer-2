2a6e0cc255be21bcef33f72deb70ae0f640e651b
Adriano Dias de Sousa <adrianodias_sma@hotmail.com>
2026-05-27 18:49:09 -0300

    Adicionar rotas para editar conta do admin

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
+const {
+  register,
+  login,
+  getAdminProfile,
+  updateAdminProfile,
+} = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/cadastro", register);
 router.post("/login", login);
+router.get("/admin-profile", getAdminProfile);
+router.put("/admin-profile", updateAdminProfile);
 
 module.exports = router;
